4d1125dccdcec45e0eea9ba497040c4bae1aeabc
Munieru <20086673+munierujp@users.noreply.github.com>
2019-11-30 20:23:48 +0900

    Add vuetify.config.js (#6)
    * Add vuetify.config.js
    
    * Add vuetifyConfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import colors from 'vuetify/es5/util/colors'
+import vuetifyConfig from './vuetify.config'
 
 export default {
   mode: 'spa',
@@ -38,34 +38,13 @@ export default {
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
-    '@nuxtjs/vuetify'
+    ['@nuxtjs/vuetify', vuetifyConfig]
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
   ],
-  /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
   /*
   ** Build configuration
   */
