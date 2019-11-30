b1182122683ba5133595977315d4829ea84e9806
Munieru <20086673+munierujp@users.noreply.github.com>
2019-11-30 20:02:52 +0900

    Introduce Sass (#5)
    * Add packages for Sass
    
    `npm i -D node-sass sass-loader`
    
    * Add stylelint-scss package
    
    `npm i -D stylelint-scss`
    
    * Add stylelint-scss to plugins
    
    * Add scss to lint-staged

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,9 @@ module.exports = {
   extends: [
     'stylelint-config-standard'
   ],
+  plugins: [
+    'stylelint-scss'
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {}
